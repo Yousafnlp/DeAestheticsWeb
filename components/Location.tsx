60d4bc15325dafe3f44606b0e5bbd205be0159ac
yousaf.ijaz <yousaf.ijaz@sparkai.ca>
2025-03-03 10:56:36 +0500

    Update location map coordinates and navbar text spacing

--- components/Location.tsx
+++ components/Location.tsx
@@ -29,8 +29,8 @@ const Location = () => {
             </div>
             <div className="rounded-lg overflow-hidden shadow-lg">
               <iframe
-               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10000!2d74.328522!3d31.534437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1700000000000" 
-               width="100%"
+             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10000!2d74.553734!3d32.513237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1709472000000" 
+             width="100%"
                 height="450"
                 style={{ border: 0 }}
                 allowFullScreen
